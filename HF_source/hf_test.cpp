4c26f2a5893656c8fbe3a77973f7d2f5a82fe501
blair <bawinogr@umich.edu>
2016-11-18 14:27:23 -0500

    cleaning up

--- HF_source/hf_test.cpp
+++ HF_source/hf_test.cpp
@@ -45,7 +45,7 @@ char *path ="Water_STO-3G";
 
 double En = hf_main(ao,occ,path);
 
-if (abs(abs(En)-74.9421)<=pow(10,-4)){cout << "Test passed" << endl;}else{cout << "Test failed" << endl;}
+if (abs(abs(En)-74.9421)<=pow(10,-4)){cout << "HF_Energy:  ... Test passed" << endl;}else{cout << "HF_Energy:  ... Test failed" << endl;}
 
 }
 
@@ -59,6 +59,6 @@ char *path = "Water_STO-3G";
 
 double En_nuc = read_nuc_en(path);
 
-if (abs(abs(En_nuc)-8.002367061810450)<=pow(10,-8)){cout << "Test passed" << endl;}else{cout << "Test failed" << endl;}
+if (abs(abs(En_nuc)-8.002367061810450)<=pow(10,-8)){cout << "Nuclear_Energy:  ...Test passed" << endl;}else{cout << "Nuclear_Energy:  ...Test failed" << endl;}
 
 };
